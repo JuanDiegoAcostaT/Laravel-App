0e666f0aeee350b05d8aa08b56d96bceece7ad6c
Juan Diego Acosta <juandiegoat01@gmail.com>
2020-08-04 16:27:09 -0500

    Routing

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("about", function () {
+    return route("about");
+})->name('about');
+
+Route::match(["get", "post"])("/", function () {
+    return "hola";
 });
+    // Route::any("/", function () {
+    //     return "hola";
+    // });
+
+    // Route::get("/about/{id?}", function ($id = 'Default') {
+    //     return "Hola Mundo ${id} ";
+    // })->where(['id' => '[0-9]+', 'id' => '[1]']);
+
+
+    // Route::get("/about/{id?}", function ($id = 'Default') {
+    //     return "Hola Mundo ${id} ";
+    // });
+;
